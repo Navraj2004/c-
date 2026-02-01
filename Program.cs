64fdbc31c26dd9465fe2c570658e0e7a0c67f371
Navraj Giri <144989391+Navraj2004@users.noreply.github.com>
2026-02-01 22:51:28 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+       int a, b;
+       char op;
+
+
+       Console.WriteLine("Enter the value of a ");
+       a=Convert.ToInt32(Console.ReadLine());
+        
+       Console.WriteLine("Enter the value of b");
+       b=Convert.ToInt32(Console.ReadLine());
+
+       Console.WriteLine("Enter the operation to perform subtraction(-) or multiplication(*) or division(/) or addition(+)");
+       op=op = Convert.ToChar(Console.ReadLine());
+
+
+
+       switch(op)
+        {
+            case '+':
+                {
+                    int ans=a+b;
+                    Console.WriteLine(" Result " + ans);
+                    break;
+                }
+            
+            case '-':
+                {
+                    int ans=a-b;
+                    Console.WriteLine(" Result " + ans);
+                    break;
+                }
+            case '*':
+                {
+                    int ans=a*b;
+                    Console.WriteLine(" Result " + ans);
+                    break;
+                }
+            case '/':
+                {
+                    if(b==0)
+                    {
+                        Console.WriteLine("Value is invalid");
+                        break;
+                    }
+             
+                    else{
+                    int ans=a/b;
+                    Console.WriteLine(ans);
+                    break;
+                    }
+                }
+
+            default:
+                   Console.WriteLine("Invalid operation");
+                   break;
+
+        }
+
+       
+
+
+
+    }
+}
